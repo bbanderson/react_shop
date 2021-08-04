732e7cadb4e6654d97ba84f39b9c9696cc5e1693
bbanderson <tombyun@naver.com>
2021-08-04 16:21:28 +0900

    Setup components

---
+++ src/components/Orders.jsx
@@ -0,0 +1,10 @@
+export default function Orders() {
+  return (
+    <aside>
+      <div className="empty">
+        <div className="title">주문 내역이 없습니다.</div>
+        <div className="description">+ 버튼을 눌러 상품을 선택해 주세요.</div>
+      </div>
+    </aside>
+  );
+}
