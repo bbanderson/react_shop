b41c25a1121581e22101159fa1d209e81621c568
bbanderson <tombyun@naver.com>
2021-08-07 09:16:12 +0900

    Add contextAPI

--- src/App.js
+++ src/App.js
@@ -3,17 +3,18 @@ import Header from "./components/Header";
 import Products from "./components/Products";
 import Orders from "./components/Orders";
 import Footer from "./components/Footer";
+import AppStateProvider from "./providers/AppStateProvider";
 
 function App() {
   return (
-    <>
+    <AppStateProvider>
       <Header />
       <div className="container">
         <Products />
         <Orders />
         <Footer />
       </div>
-    </>
+    </AppStateProvider>
   );
 }
 
